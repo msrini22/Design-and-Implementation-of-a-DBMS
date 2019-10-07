4c1e5405ada903626a9b9ed6c30a3e015ed6186c
msrini22 <49651475+msrini22@users.noreply.github.com>
2019-10-06 18:19:19 -0700

    Add files via upload

---
+++ DBMSI phase 3/phase 3/minibase_repo/iterator/Operand.java
@@ -0,0 +1,12 @@
+package iterator;
+import global.CompositeType;
+import global.IntervalType;
+
+public class Operand {
+  public  FldSpec  symbol;
+  public  String   string;
+  public  int      integer;
+  public  float    real;
+  public  IntervalType interval;
+  public CompositeType composite;
+}
